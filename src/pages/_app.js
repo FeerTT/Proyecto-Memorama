3a02e7e8b6ca6f3644a115509b8092bbef3b9440
FeerTT <fernandotrillobaron25@hotmail.com>
2023-10-18 04:33:30 -0300

    subida del proyecto a github

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,6 @@
 import '@/styles/globals.css'
+import Modal from 'react-modal'
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,244 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React,{useState} from "react"
+import Board from "@/components/board"
+import Card from "@/components/cards"
+import Modal from "react-modal";
+import ModalPlayers from "@/components/modalPlayers";
+import { useEffect } from "react";
+import SocialMediaBox from "@/components/redes";
 
-const inter = Inter({ subsets: ['latin'] })
+export default function Memorama() {
+  const [showModal, setShowModal] = useState(false);
+  const [cards, setCards] = useState([
+    { id: 1, value: 'A', isFlipped: false, image:'/img/img1.jpg'  },
+    { id: 2, value: 'B', isFlipped: false, image: '/img/img2.jpg' },
+    { id: 3, value: 'A', isFlipped: false, image:'/img/img1.jpg'  },
+    { id: 4, value: 'B', isFlipped: false, image: '/img/img2.jpg' },
+    { id: 5, value: 'C', isFlipped: false, image: '/img/img3.jpg' },
+    { id: 6, value: 'C', isFlipped: false, image: '/img/img3.jpg' },
+    { id: 7, value: 'D', isFlipped: false, image: '/img/img4.jpg' },
+    { id: 8, value: 'D', isFlipped: false, image: '/img/img4.jpg' },
+    { id: 9, value: 'E', isFlipped: false, image: '/img/img5.jpg' },
+    { id: 10, value: 'E', isFlipped: false, image: '/img/img5.jpg' },
+    { id: 11, value: 'F', isFlipped: false, image: '/img/img6.jpg' },
+    { id: 12, value: 'F', isFlipped: false, image: '/img/img6.jpg' },
+    { id: 13, value: 'G', isFlipped: false, image: '/img/img7.jpg' },
+    { id: 14, value: 'G', isFlipped: false, image: '/img/img7.jpg' },
+    { id: 15, value: 'H', isFlipped: false, image: '/img/img8.jpg' },
+    { id: 16, value: 'H', isFlipped: false, image: '/img/img8.jpg' },
+    { id: 17, value: 'I', isFlipped: false, image: '/img/img9.jpg' },
+    { id: 18, value: 'I', isFlipped: false, image: '/img/img9.jpg' },
+    { id: 19, value: 'J', isFlipped: false, image: '/img/img10.jpg' },
+    { id: 20, value: 'J', isFlipped: false, image: '/img/img10.jpg' },
+    { id: 21, value: 'K', isFlipped: false, image: '/img/img11.jpg' },
+    { id: 22, value: 'K', isFlipped: false, image: '/img/img11.jpg' },
+    { id: 23, value: 'L', isFlipped: false, image: '/img/img12.jpg' },
+    { id: 24, value: 'L', isFlipped: false, image: '/img/img12.jpg' },
+    { id: 25, value: 'M', isFlipped: false, image: '/img/img13.jpg' },
+    { id: 26, value: 'M', isFlipped: false, image: '/img/img13.jpg' },
+    { id: 27, value: 'N', isFlipped: false, image: '/img/img14.jpg' },
+    { id: 28, value: 'N', isFlipped: false, image: '/img/img14.jpg' },
+    { id: 29, value: 'O', isFlipped: false, image: '/img/img15.jpg' },
+    { id: 30, value: 'O', isFlipped: false, image: '/img/img15.jpg' },
+  ]);
+  const [selectedCards, setSelectedCards] = useState([]);
 
-export default function Home() {
+  const [players, setPlayers] = useState([
+    // { id: 1, name: 'Player 1', score: 0, isTurn: true },
+    // { id: 2, name: 'Player 2', score: 0, isTurn: false },
+    // { id: 3, name: 'Player 3', score: 0, isTurn: false },
+    // { id: 4, name: 'Player 4', score: 0, isTurn: false },
+  ]);
+  const [currentPlayer, setCurrentPlayer] = useState(0);
+  const [numPlayers, setNumPlayers] = useState(1);
+  const [winner, setWinner] = useState(null);
+
+ 
+  useEffect(() => {
+    
+    // mezclarCartas();
+  }, []);
+
+  const switchTurns = () => {
+    setPlayers(prevPlayers =>
+      prevPlayers.map((player, index) => ({
+        ...player,
+        isTurn: index === currentPlayer ? false : index === nextPlayerIndex ? true : player.isTurn,
+      }))
+    );
+
+   
+    const nextPlayerIndex = (currentPlayer + 1) % players.length;
+
+  
+    setCurrentPlayer(nextPlayerIndex);
+  };
+
+  const handleSelectPlayers = (selectedPlayers) => {
+    setNumPlayers(selectedPlayers);
+    const newPlayers = Array.from({ length: selectedPlayers }, (_, index) => ({
+      id: index + 1,
+      name: `Player ${index + 1}`,
+      score: 0,
+      isTurn: index === 0, 
+    }));
+
+    setPlayers(newPlayers);
+  };
+
+
+  const handleCardClick = (clickedCard) => {
+    console.log('Clic en la carta', clickedCard);
+  
+  
+    if (selectedCards.some((card) => card.id === clickedCard.id)) {
+      return; 
+    }
+  
+    if (clickedCard.isFlipped || clickedCard.isMatched) {
+      return; 
+    }
+    if (selectedCards.length === 2) {
+      return; 
+    }
+    
+    setCards((prevCards) =>
+      prevCards.map((card) =>
+        card.id === clickedCard.id ? { ...card, isFlipped: true } : card
+      )
+    );
+    setSelectedCards((prevSelectedCards) => [...prevSelectedCards, clickedCard]);
+    if (selectedCards.length === 1) {
+      const [firstCard] = selectedCards;
+      if (firstCard.value === clickedCard.value) {
+        setTimeout(() => {
+          setCards((prevCards) =>
+            prevCards.map((card) =>
+              card.id === firstCard.id || card.id === clickedCard.id
+                ? { ...card, isMatched: true }
+                : card
+            )
+          );
+          setPlayers((prevPlayers) =>
+          prevPlayers.map((player) =>
+            player.isTurn ? { ...player, score: player.score + 1 } : player
+          )
+        );
+          setCards((prevCards) => {
+            const allMatched = prevCards.every((card) => card.isMatched);
+            if (allMatched) {
+              findWinner();
+              setShowModal(true);
+            }
+            return prevCards;
+          });
+          
+          setSelectedCards([]);
+        }, 1000); 
+      } else {
+        
+        setTimeout(() => {
+          setCards((prevCards) =>
+            prevCards.map((card) =>
+              card.id === firstCard.id || card.id === clickedCard.id
+                ? { ...card, isFlipped: false }
+                : card
+            )
+          );
+          
+          setSelectedCards([]);
+          switchTurns();
+        }, 1000); 
+      }
+    }
+  };
+
+
+  const mezclarCartas = () => {
+    const cartasMezcladas = [...cards].sort(() => Math.random() - 0.5);
+    setCards(cartasMezcladas);
+  };
+
+
+  const closeModal =(event) =>{
+    if (event){
+      setShowModal(false);
+    }
+  }
+
+  const reiniciarJuego = () => {
+    const cartasMezcladas = [...cards].sort(() => Math.random() - 0.5);
+
+   
+    const cartasOcultas = cartasMezcladas.map(card => ({ ...card, isFlipped: false }));
+    
+    setCards(cartasOcultas);
+    const jugadoresReiniciados = players.map((jugador) => ({
+      ...jugador,
+      score: 0,
+    }));
+    setPlayers(jugadoresReiniciados);
+    setWinner(null);
+
+  };
+  const findWinner = () => {
+    // Encuentra al jugador que tiene isTurn: true
+    const currentPlayer = players.find(player => player.isTurn);
+  
+    // Suma 1 punto al jugador actual si existe
+    if (currentPlayer) {
+      const updatedPlayers = players.map(player =>
+        player.id === currentPlayer.id
+          ? { ...player, score: player.score + 1 }
+          : player
+      );
+  
+      // Actualiza el estado de los jugadores con los puntos actualizados
+      setPlayers(updatedPlayers);
+    }
+  
+    // Encuentra al ganador
+    const gameWinner = players.reduce((max, player) =>
+      player.score > max.score ? player : max,
+      players[0]
+    );
+  
+    // Actualiza el estado del ganador
+    setWinner(gameWinner);
+  };
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <main className="space" role="img" aria-label="cartoon to represent space (an animated astronaut floating away)">
+      
+    <div className="text-center mt-8">
+    <h1 className="text-3xl font-bold mb-6">Memory Game</h1>
+    
+    
+  <ModalPlayers onSelectPlayers={handleSelectPlayers}/>
+        <div className="player-list">
+        {players.map((player) => (
+          <div key={player.id} className="player">
+            <p className="player-name">{`${player.name}'s Turn`}</p>
+            <p className="player-score">Score: {player.score}</p>
+          </div>
+        ))}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <button className="bg-blue-500 hover:bg-blue-700 text-white mb-10 font-bold py-2 px-4 rounded border border-blue-700" onClick={reiniciarJuego}>Restart Game</button>
+      <Board cards={cards} onCardClick={handleCardClick}></Board>
+    </div>
+    <Modal
+        isOpen={showModal}
+        onRequestClose={closeModal}
+        contentLabel="¡Felicidades! Has completado el juego."
+        className="modal"
+        overlayClassName="overlay"
+      >
+        <h2 className="h2Ganador">¡Congratulations to: {winner && winner.name} for winning the game!</h2>
+        <button className="close-button" onClick={closeModal}>Close</button>
+      </Modal>
+      <footer>
+      <p className="text-center mt-8" style={{ fontFamily: 'Montserrat, sans-serif', color: '#888', fontSize: '14px' }}>Website designed and developed by: Fernando T. Trillo</p>
+      <SocialMediaBox></SocialMediaBox>
+      </footer>
     </main>
   )
 }
